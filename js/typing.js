65e70878f8cefd706bb2d8921a9a62c07c53de81
MarcoKouyate <marco.kouyate@yahoo.fr>
2019-03-03 20:41:10 +0100

    Skeleton + cover

--- js/typing.js
+++ js/typing.js
@@ -51,7 +51,7 @@ var TxtType = function(el, toRotate, period) {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em dotted #343a40 }";
         document.body.appendChild(css);
     };
 
